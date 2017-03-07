46bbfab6e8dfa7321d5ac707bfbeaf636ff9ffef
Toufik <toufikharbeche@hotmail.fr>
2017-03-07 16:54:51 +0100

    Ajout de la route avec modif du controller DefaultController et de la page test-twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,15 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/twig-test", name="app_twig_test")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        return $this->render(':default:index.html.twig', [
+            'name' => 'john',
+            'days' => array('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'),
+            'html' => '<b> Ce texte n\'est pas en gras !</b>',
         ]);
     }
 }
